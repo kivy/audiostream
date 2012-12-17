7b047b0e4fa65d95b699ca304e71498f055763ec
Mathieu Virbel <mat@kivy.org>
2012-12-17 16:43:47 +0100

    setup: rework ios/android detection from the latest approach done in kivy

--- setup.py
+++ setup.py
@@ -1,18 +1,34 @@
+import sys
 import os
-from os.path import join, realpath
+from os.path import join
 from os import environ
 from distutils.core import setup
 from distutils.extension import Extension
-try:
-    from Cython.Distutils import build_ext
-    have_cython = True
-    cmdclass = { 'build_ext': build_ext }
-except ImportError:
-    print "*"*20, "NO CYTHON"
-    have_cython = False
-    cmdclass = {}
 
+# detect Python for android project (http://github.com/kivy/python-for-android)
+# or kivy-ios (http://github.com/kivy/kivy-ios)
+platform = sys.platform
+ndkplatform = environ.get('NDKPLATFORM')
+if ndkplatform is not None and environ.get('LIBLINK'):
+    platform = 'android'
+kivy_ios_root = environ.get('KIVYIOSROOT', None)
+if kivy_ios_root is not None:
+    platform = 'ios'
 
+# ensure Cython is installed for desktop app
+have_cython = False
+if platform in ('android', 'ios'):
+    print 'Cython import ignored'
+else:
+    try:
+        from Cython.Distutils import build_ext
+        have_cython = True
+        cmdclass = {'build_ext': build_ext}
+    except ImportError:
+        print '**** Cython is required to compile audiostream ****'
+        raise
+
+# configure the env
 libraries = ['SDL', 'SDL_mixer']
 library_dirs = []
 include_dirs = ['/usr/include/SDL']
@@ -29,16 +45,15 @@ else:
 
 # scan the 'dvedit' directory for extension files, converting
 # them to extension names in dotted notation
-def scandir(dir, files=[]):
-    for file in os.listdir(dir):
-        path = os.path.join(dir, file)
+def scandir(directory, files=[]):
+    for fn in os.listdir(directory):
+        path = join(directory, fn)
         if os.path.isfile(path) and path.endswith(".pyx"):
             path = path.replace('.pyx', '.c')
             files.append(path.replace(os.path.sep, ".")[:-2])
         elif os.path.isdir(path):
             scandir(path, files)
     return files
-
 
 # generate an Extension object from its dotted name
 def makeExtension(extName):
