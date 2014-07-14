1e14af1594984c7bd976b69954ff5d2b4c07c051
Peter Bouda <pbouda@live.de>
2014-07-14 16:09:07 +0100

    Removed 32-bit encoding.

--- audiostream/core.pyx
+++ audiostream/core.pyx
@@ -204,7 +204,7 @@ class AudioInput(object):
     def __init__(self, callback=None, source='default', rate=44100, channels=1,
             buffersize=-1, encoding=16):
         super(AudioInput, self).__init__()
-        if encoding not in (8, 16, 32):
+        if encoding not in (8, 16):
             raise Exception('Invalid encoding, must be one of 8, 16')
         if channels not in (1, 2):
             raise Exception('Invalid channels, must be one of 1, 2')
